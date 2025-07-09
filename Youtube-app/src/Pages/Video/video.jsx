d21d0d814d65db1dc5a783867a8dbe0a775af54b
Gajapathi K <113296010+ViratGaja@users.noreply.github.com>
2025-07-09 21:46:53 +0530

    LEARN

--- Youtube-app/src/Pages/Video/video.jsx
+++ Youtube-app/src/Pages/Video/video.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './video.css'
 import Playvideo from '../../Components/Playvideo/Playvideo'
+import Recommended from '../../Components/Recommended/Recommended'
 const video = () => {
   return (
     <div className='play-container'>
       <Playvideo/>
+      <Recommended/>
       
     </div>
   )
